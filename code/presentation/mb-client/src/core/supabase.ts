6c5d3f3f99c9443ac20c9f1ec2758714a81495ee
Adam Saudagar <adsau59@gmail.com>
2023-12-04 22:37:14 +0530

    #23 created google signin component

---
+++ code/presentation/mb-client/src/core/supabase.ts
@@ -0,0 +1,10 @@
+import 'react-native-url-polyfill/auto'
+
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  //todo wip/auth dont commit these stuff
+  '',
+  ''
+);
--- code/presentation/mb-client/src/screens/login/index.tsx
+++ code/presentation/mb-client/src/screens/login/index.tsx
@@ -1,24 +1,56 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 import React from 'react';
 
 import { useAuth } from '@/core';
-import { useSoftKeyboardEffect } from '@/core/keyboard';
 import { FocusAwareStatusBar } from '@/ui';
 
-import type { LoginFormProps } from './login-form';
-import { LoginForm } from './login-form';
+import { supabase } from '../../core/supabase';
 
 export const Login = () => {
   const signIn = useAuth.use.signIn();
-  useSoftKeyboardEffect();
+  // useSoftKeyboardEffect();
 
-  const onSubmit: LoginFormProps['onSubmit'] = (data) => {
-    console.log(data);
-    signIn({ access: 'access-token', refresh: 'refresh-token' });
-  };
+  GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+    webClientId: 'YOUR CLIENT ID FROM GOOGLE CONSOLE',
+  });
   return (
     <>
       <FocusAwareStatusBar />
-      <LoginForm onSubmit={onSubmit} />
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={async () => {
+          try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            if (userInfo.idToken) {
+              const { data, error } = await supabase.auth.signInWithIdToken({
+                provider: 'google',
+                token: userInfo.idToken,
+              });
+              console.log(error, data);
+              signIn({ access: 'access-token', refresh: 'refresh-token' });
+            } else {
+              throw new Error('no ID token present!');
+            }
+          } catch (error: any) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+              // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+              // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+              // play services not available or outdated
+            } else {
+              // some other error happened
+            }
+          }
+        }}
+      />
     </>
   );
 };
