32749b002dd87af2c78810a367edac1b44af34c5
Adam Saudagar <adsau59@gmail.com>
2023-12-17 07:19:42 +0530

    #28 now also fetching namaz timing for each of the masjids using database function in supabase

--- code/presentation/mb-client/src/api/masjid/types.ts
+++ code/presentation/mb-client/src/api/masjid/types.ts
@@ -3,13 +3,24 @@ export type Masjid = {
   name: string;
   address: Address;
   mobile_no: string;
-  plus_code: string;
+  longitude: number;
+  latitude: number;
+  namaz_timings: NamazTimings;
 };
 
 type Address = {
   line1: string;
   line2: string;
   pin: string;
+};
+
+type NamazTimings = {
+  fajar: string;
+  zohar: string;
+  jummah: string;
+  asr: string;
+  magrib: string;
+  isha: string;
 };
 
 export const AddressString = (a: Address) =>
--- code/presentation/mb-client/src/api/masjid/use-masjids.ts
+++ code/presentation/mb-client/src/api/masjid/use-masjids.ts
@@ -6,7 +6,8 @@ export const useMasjids = () => {
   return useQuery({
     queryKey: ['masjids'],
     queryFn: async () => {
-      const result = await supabase.from('masjid').select('*');
+      const result = await supabase.rpc('get_masjids_with_namaz_timings');
+      console.log(`result: ${JSON.stringify(result)}`);
       const { data, error: _err } = result;
       return data;
     },
